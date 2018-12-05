4715fc575385353b902f7f77434dc8003ea307eb
Karl <karl.hopkinsonturrell@pearson.com>
2018-12-05 18:47:01 +0000

    Add ECR and instance role integration for Anchore

--- anchore/root.tf
+++ anchore/root.tf
@@ -15,6 +15,52 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"]
 }
 
+# Instance role with ECR permissions
+resource "aws_iam_role" "anchore_ecr_role" {
+    name = "anchore_ecr_role"
+
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "anchore_ecr_policy" {
+    name = "anchore_ecr_policy"
+    role = "${aws_iam_role.anchore_ecr_role.id}"
+
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "anchore_instance_profile" {
+    name = "anchore_instance_profile"
+    role = "${aws_iam_role.anchore_ecr_role.name}"
+}
+
 # AWS instance
 resource "aws_instance" "anchore_instance" {
     ami = "${data.aws_ami.ubuntu.id}"
@@ -22,6 +68,9 @@ resource "aws_instance" "anchore_instance" {
     key_name = "${var.key_pair_name}"
 
     user_data = "${file("anchore/user_data.sh")}"
+
+    # Give Anchore access to ECR via EC2 instance profile
+    iam_instance_profile = "${aws_iam_instance_profile.anchore_instance_profile.name}"
 
     security_groups = ["${var.security_group_name}"]
 
